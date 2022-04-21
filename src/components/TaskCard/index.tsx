95b5918cb9a96b3f8421cf53687bbbcc4cd09c0b
masatojpn <masatojpn@icloud.com>
2022-04-22 05:55:43 +0900

    ２階層のフォームを作成

--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -1,89 +1,66 @@
 import React from 'react';
 import { useFieldArray } from 'react-hook-form';
 
-
-const projectLists = [
-	{value: '1', name: 'プロジェクトA'},
-	{value: '1', name: 'プロジェクトA'},
-	{value: '1', name: 'プロジェクトA'},
-	{value: '1', name: 'プロジェクトA'},
-	{value: '1', name: 'プロジェクトA'},
-	{value: '1', name: 'プロジェクトA'},
-]
-
-const workTimes = [
-	0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6
-]
-
-const categories = [
-	{value: '1', name: 'フロントエンド'},
-	{value: '2', name: 'バックエンド'},
-	{value: '3', name: 'インフラ'},
-	{value: '4', name: 'デザイン'},
-]
-
-const tags = [
-	{value: 'typescript', name: 'TypeScript'},
-	{value: 'typescript', name: 'TypeScript'},
-	{value: 'typescript', name: 'TypeScript'},
-	{value: 'typescript', name: 'TypeScript'},
-	{value: 'typescript', name: 'TypeScript'},
-	{value: 'typescript', name: 'TypeScript'},
-]
-
-export const ProjectCard = ( {control, register, setValue, getValues, defaults }:any) => {
+export const TaskCard = ( { projectIndex, control, register }:any) => {
 
 	const { fields, append, remove, prepend } = useFieldArray({
 		control,
-		name: 'tasks'
+		name: `posts.projects.${projectIndex}.tasks`
 	});
 
 	return(
-		<>
-			<ul>
-				{fields.map((task, taskIndex) => {
-					return(
-						<li key={taskIndex}>
-							<select {...register(`tasks[${taskIndex}].projectName`)}>
-								<option value="">プロジェクトを選択</option>
-								{projectLists.map((projectList, projectListIndex) => {
-									return(
-										<option key={projectListIndex} value={projectList.value}>{projectList.name}</option>
-									)
-								})}
-							</select>
-							<textarea {...register(`tasks[${taskIndex}].taskTitle`)} placeholder="作業内容を入力"></textarea>
-							<select>
-								<option value="">作業時間を選択</option>
-								{workTimes.map((workTime, workTimeIndex) => {
-									return(
-										<option value={workTimeIndex}>{workTime}</option>
-									)
-								})}
-							</select>
-							<select>
-								<option value="">作業カテゴリーを選択</option>
-								{categories.map((category, categoryIndex) => {
-									return(
-										<option value={category.value}>{category.name}</option>
-									)
-								})}
-							</select>
-							{tags.map((tag, tagIndex) => {
-								return(
-									<>
-										<input type="checkbox" value={tag.value} {...register(`tasks[${taskIndex}].tags[]`)} />{tag.name}
-									</>
-								)
-							})}
-							<div>
-								<button type="button" onClick={() => remove(taskIndex)}>タスクを削除</button>
-							</div>
-						</li>
-					)
-				})}
-			</ul>
-			<button type="button" onClick={() => append({})}>タスクを追加</button>
-		</>
-	)
+    <div className="space-y-10">
+      {fields.map((task, taskIndex) => {
+        return(
+          <div className="relative block rounded-lg shadow-md w-full p-6 bg-slate-100">
+            <div className="flex w-full gap-4">
+              <div className="w-3/4">
+                <textarea
+                  className="block w-full rounded-md border-0 p-4 h-full text-sm font-mono"
+                  {...register(`posts.projects.${projectIndex}.tasks.${taskIndex}.taskTitle`, {
+                  required: {
+                    value: true,
+                    message: 'タスクを入力してください',
+                  }
+                })} />
+              </div>
+              <div className="w-1/4 space-y-4">
+                <input
+                  className="block w-full rounded-md border-0 p-4 text-sm font-mono"
+                  type="number"
+                  step={0.5}
+                  max={12}
+                  {...register(`posts.projects.${projectIndex}.tasks.${taskIndex}.category`, {
+                  required: {
+                    value: true,
+                    message: '作業時間',
+                  }
+                })} />
+                <input
+                  className="block w-full rounded-md border-0 p-4 text-sm font-mono"
+                  type="text"
+                  {...register(`posts.projects.${projectIndex}.tasks.${taskIndex}.category`, {
+                  required: {
+                    value: true,
+                    message: 'タスクを入力してください',
+                  }
+                })} />
+              </div>
+            </div>
+
+            <button
+              className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2"
+              type="button"
+              onClick={() => remove(taskIndex)}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </button>
+          </div>
+        );
+      })}
+      <button className="mt-10 bg-black text-white py-3 w-40 block text-sm rounded-md" type="button" onClick={()=> append({})}>タスクを追加</button>
+    </div>
+  );
 }
--- src/containers/Form/index.tsx
+++ src/containers/Form/index.tsx
@@ -1,130 +1,154 @@
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, useFieldArray } from 'react-hook-form';
+import { TaskCard } from '@components/TaskCard';
 
 // -------------------------------------
 // model
 // -------------------------------------
-type FormInputs = {
-	projectTitle: string,
-	taskTitle: string,
-	category: string,
-	tags: string[],
-	workTime: string
+type IPosts = {
+  postDate: string,
+  projects: [
+    {
+      projectTitle: string,
+      tasks: [
+        {
+          taskTitle: string,
+          category: string
+        }
+      ]
+    }
+  ]
 }
 
 // -------------------------------------
 // 初期値を設定
 // -------------------------------------
-const defaultValues: FormInputs = {
-	projectTitle: '1',
-	taskTitle: '作業内容',
-	category: '1',
-	tags: ['1'],
-	workTime: '1'
+const defaultValues: IPosts = {
+  postDate: '2020-12-31',
+  projects: [
+    {
+      projectTitle: 'プロジェクトタイトル',
+      tasks: [
+        {
+          taskTitle: '作業内容',
+          category: '作業カテゴリ',
+        }
+      ]
+    }
+  ]
 }
 
-const projectMap = new Map<string, string>([['1', 'プロジェクトA'], ['2', 'プロジェクトB'], ['3', 'プロジェクトC']]);
-const categoryMap = new Map<string, string>([['1', 'フロントエンド'], ['2', 'バックエンド'], ['3', 'インフラ'], ['4', 'デザイン']]);
-const tagMap = new Map<string, string>([['1', 'TypeScript'], ['2', 'jQuery'], ['3', 'JavaScript'], ['4', 'PHP'], ['5', 'Laravel']]);
+// const projectMap = new Map<string, string>([['1', 'プロジェクトA'], ['2', 'プロジェクトB'], ['3', 'プロジェクトC']]);
+// const categoryMap = new Map<string, string>([['1', 'フロントエンド'], ['2', 'バックエンド'], ['3', 'インフラ'], ['4', 'デザイン']]);
+// const tagMap = new Map<string, string>([['1', 'TypeScript'], ['2', 'jQuery'], ['3', 'JavaScript'], ['4', 'PHP'], ['5', 'Laravel']]);
 
 export const Form = () => {
 
-	// 送信データの型チェック
-	// genericsで型を指定する(type FormInputs)
-	const [submitData, setData] = useState<FormInputs>()
+  const data = {
+    postDate: '2020-12-31',
+    projects: [
+      {
+      projectTitle: 'プロジェクトタイトル',
+        tasks: [
+          {
+            taskTitle: '作業内容',
+            category: '作業カテゴリ',
+          }
+        ]
+      }
+    ]
+  };
+  // 送信データの型チェック
+  // genericsで型を指定する(type FormInputs)
+  const [submitData, setData] = useState<IPosts>()
 
-	// register / handleSubmitをuseFormで初期化
-	// genericsで型を指定する
-	// 初期値をセットする場合useForm関数内でdefaultValuesオブジェクトに変数を指定する
-	const {
-		register, handleSubmit, formState:{ errors }
-	} = useForm<FormInputs>({
-		defaultValues: defaultValues
-	})
+  // register / handleSubmitをuseFormで初期化
+  // genericsで型を指定する
+  // 初期値をセットする場合useForm関数内でdefaultValuesオブジェクトに変数を指定する
+  const {
+    control, register, handleSubmit, formState:{ errors }, getValues, setValue
+  } = useForm({
+    defaultValues: { posts: data }
+  });
 
-	// フォーム送信時の型チェック
-	// 引数に名前と型を指定
-	const onSubmit = (data: FormInputs) => {
-		setData(data)
-		console.log(data);
-	};
+  const {
+    fields,
+    append,
+    remove
+  } = useFieldArray({
+    control,
+    name: 'posts.projects'
+  });
 
-	return(
-		<>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<label>
-					プロジェクト
-					{/*
-						フォーム要素にスプレッドでregister関数を展開
-						第一引数には input などのname属性（先にtype hogehoge = {} で定義）
-					 */}
-					<select {...register('projectTitle', {
-						required: {
-							value: true,
-							message: 'プロジェクトを選択してください',
-						}
-					})}>
-						<option value="1">プロジェクトA</option>
-						<option value="2">プロジェクトB</option>
-						<option value="3">プロジェクトC</option>
-						<option value="4">プロジェクトD</option>
-					</select>
-					{errors.projectTitle && <span className="error">{errors.projectTitle.message}</span>}
-				</label>
+  // フォーム送信時の型チェック
+  // 引数に名前と型を指定
+  const onSubmit = (data: any) => {
+    setData(data);
+  };
 
-				<label>
-					作業内容
-					<textarea {...register('taskTitle', {
-						required: {
-							value: true,
-							message: '作業内容を入力してください',
-						}
-					})} />
-					{errors.taskTitle && <span className="error">{errors.taskTitle.message}</span>}
-				</label>
+  return(
+    <>
+      <form className="p-8" onSubmit={handleSubmit<IPosts>(onSubmit)}>
+        <input
+          className="block w-40 mb-4 rounded-md bg-slate-100 border-none mb-10 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          type="date"
+          {...register(`posts.postDate`, {
+          required: {
+            value: true,
+            message: '入力してください'
+          }
+        })} />
+        <div className="space-y-10">
+        {fields.map((project, projectIndex) => {
+          return(
+            <div className="relative p-6 rounded-sm bg-slate-50 shadow-md" key={projectIndex}>
+              <label className="flex w-full text-xs items-center">
+                <div className="w-96">
+                  <input
+                    type="text"
+                    className="block w-full rounded-md border-none bg-white mb-5 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                    {...register(`posts.projects.${projectIndex}.projectTitle`, {
+                      required: {
+                        value: true,
+                        message: 'プロジェクトを選択してください',
+                      }
+                    })}
+                  />
+                </div>
+              </label>
+              <TaskCard projectIndex={projectIndex} {...{ control, register }} />
+              <button
+                className="absolute top-0 right-0 translate-x-2/4 -translate-y-1/2"
+                type="button"
+                onClick={() => remove(projectIndex)}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </button>
+            </div>
+          )
+        })}
+        </div>
+        <div className="mt-10 inline-flex space-x-4">
+          <button className="text-xs block w-24 py-3 bg-emerald-100 text-gray-800 rounded-sm shadow-md" type="button" onClick={()=> {
+            setValue('posts.projects', [
+              ...getValues().posts.projects,
+              {
+                projectTitle: 'プロジェクト名',
+                tasks: [
+                  {
+                    taskTitle: 'タスク名',
+                    category: 'カテゴリー'
+                  }
+                ]
+              }
+            ])
+          }}>追加</button>
+          <button className="text-xs block w-24 py-3 bg-emerald-800 rounded-sm shadow-md text-white" type="submit">送信</button>
+        </div>
+      </form>
+    </>
 
-				<label>
-					性別
-					<input type="radio" {...register('category', { required: true })} value={1}/>フロントエンド
-					<input type="radio" {...register('category', { required: true })} value={2}/>バックエンド
-					<input type="radio" {...register('category', { required: true })} value={3}/>インフラ
-					<input type="radio" {...register('category', { required: true })} value={4}/>デザイン
-				</label>
-
-				<label>
-					<input type="checkbox" {...register('tags', { required: true })} value={1}/>TypeScript
-				</label>
-				<label>
-					<input type="checkbox" {...register('tags', { required: true })} value={2}/>jQuery
-				</label>
-				<label>
-					<input type="checkbox" {...register('tags', { required: true })} value={3}/>JavaScript
-				</label>
-				<label>
-					<input type="checkbox" {...register('tags', { required: true })} value={4}/>PHP
-				</label>
-				<label>
-					<input type="checkbox" {...register('tags', { required: true })} value={5}/>Laravel
-				</label>
-
-				<input type="submit" />
-			</form>
-			<h2>Send Data</h2>
-			{submitData
-				? <>
-						<p>{projectMap.get(submitData.projectTitle)}</p>
-						<p>{submitData.taskTitle}</p>
-						<p>{categoryMap.get(submitData.category)}</p>
-						{submitData.tags.map((tag, k) => {
-							return(
-								<p>{tagMap.get(tag)}</p>
-							)})
-						}
-					</>
-				: 'Not send yet'
-			}
-			<h3>Errors</h3>
-		</>
-
-	)
+  )
 }
